4211d905106f04cb96ad882275bebe28fd895b04
Jonathan Pyers <jon@pye.rs>
2019-01-04 13:17:06 -0800

    [fix] Description shows nicely on PyPi now

--- setup.py
+++ setup.py
@@ -2,10 +2,19 @@ import sys
 
 from setuptools import setup
 
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 options = dict(
     name='booker',
-    version='1.0.0',
+    version='1.0.1',
     license='MIT',
+    description='Python task scheduling with a simple, English syntax.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='nvms',
     author_email='jon@pye.rs',
     packages=['booker'],
